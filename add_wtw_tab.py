4e9eea9c61b4f69846562809610fbc88c9878c2b
James Savage <james.savage@walmart.com>
2026-02-09 12:54:42 -0600

    Refresh WTW data 2026-02-09 12:53 - fresh BQ pull, 5204 WOs

--- add_wtw_tab.py
+++ add_wtw_tab.py
@@ -9,7 +9,7 @@ from datetime import datetime
 
 # Paths
 DASHBOARD_PATH = Path(__file__).parent / 'index.html'
-WTW_DATA_PATH = Path.home() / 'bigquery_results' / 'wtw-pm-scores-fresh-latest-20260209-083424.csv'
+WTW_DATA_PATH = Path.home() / 'bigquery_results' / 'wtw-fy26-workorders-pm-scores-20260209-125320.csv'
 
 def load_csv(path):
     """Load CSV file and return list of dicts"""
